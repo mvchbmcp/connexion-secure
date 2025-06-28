e3c9fea71ac16d15707a56e10e4071f72f63ede8
mvchbmcp <claveriemorgane0@gmail.com>
2025-06-28 18:37:41 +0200

    Update supabaseClient.js

--- netlify/functions/supabaseClient.js
+++ netlify/functions/supabaseClient.js
@@ -1,7 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://dwrropzvxdykvdiuvlnp.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3cnJvcHp2eGR5a3ZkaXV2bG5wIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MTExNTM4MywiZXhwIjoyMDY2NjkxMzgzfQ.Cd5GKM9P6Q9oAJiyTUz0XmVAADzO7256Uiy_x3KOhcI';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
