383a5a847e7a7a1db4e4f18be5598ef2d602f6f6
mvchbmcp <claveriemorgane0@gmail.com>
2025-06-28 17:40:58 +0200

    Create supabaseClient.js

---
+++ netlify/functions/supabaseClient.js
@@ -0,0 +1,7 @@
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = 'https://dwrropzvxdykvdiuvlnp.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3cnJvcHp2eGR5a3ZkaXV2bG5wIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MTExNTM4MywiZXhwIjoyMDY2NjkxMzgzfQ.Cd5GKM9P6Q9oAJiyTUz0XmVAADzO7256Uiy_x3KOhcI';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+module.exports = supabase;
